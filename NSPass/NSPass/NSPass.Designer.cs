38921e81bc953760cdbdf199c55d38741028b595
Joswin John <joswinn15@gmail.com>
2022-01-10 14:53:12 -0500

    Initial commit

---
+++ NSPass/NSPass/NSPass.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace NSPass
+{
+    partial class NSPass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.onButton = new System.Windows.Forms.Button();
+            this.domainBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.consoleBox = new System.Windows.Forms.RichTextBox();
+            this.serverBox = new System.Windows.Forms.TextBox();
+            this.portBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.blockCheck = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // onButton
+            // 
+            this.onButton.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onButton.Location = new System.Drawing.Point(688, 25);
+            this.onButton.Name = "onButton";
+            this.onButton.Size = new System.Drawing.Size(73, 46);
+            this.onButton.TabIndex = 0;
+            this.onButton.Text = "CHECK";
+            this.onButton.UseVisualStyleBackColor = true;
+            this.onButton.Click += new System.EventHandler(this.onButton_Click);
+            // 
+            // domainBox
+            // 
+            this.domainBox.Location = new System.Drawing.Point(274, 51);
+            this.domainBox.Name = "domainBox";
+            this.domainBox.Size = new System.Drawing.Size(408, 20);
+            this.domainBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 42);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "NSPass";
+            // 
+            // consoleBox
+            // 
+            this.consoleBox.Location = new System.Drawing.Point(42, 104);
+            this.consoleBox.Name = "consoleBox";
+            this.consoleBox.ReadOnly = true;
+            this.consoleBox.Size = new System.Drawing.Size(719, 293);
+            this.consoleBox.TabIndex = 6;
+            this.consoleBox.Text = "";
+            // 
+            // serverBox
+            // 
+            this.serverBox.Location = new System.Drawing.Point(274, 25);
+            this.serverBox.Name = "serverBox";
+            this.serverBox.Size = new System.Drawing.Size(303, 20);
+            this.serverBox.TabIndex = 7;
+            // 
+            // portBox
+            // 
+            this.portBox.Location = new System.Drawing.Point(613, 27);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(69, 20);
+            this.portBox.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(189, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "NSPass Server";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(189, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Domain Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(583, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Port";
+            // 
+            // blockCheck
+            // 
+            this.blockCheck.AutoSize = true;
+            this.blockCheck.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blockCheck.Location = new System.Drawing.Point(323, 82);
+            this.blockCheck.Name = "blockCheck";
+            this.blockCheck.Size = new System.Drawing.Size(163, 19);
+            this.blockCheck.TabIndex = 12;
+            this.blockCheck.Text = "Domain Blocked Check";
+            this.blockCheck.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(586, 403);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(175, 35);
+            this.saveButton.TabIndex = 13;
+            this.saveButton.Text = "Save New Hosts File";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // NSPass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 450);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.blockCheck);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.portBox);
+            this.Controls.Add(this.serverBox);
+            this.Controls.Add(this.consoleBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.domainBox);
+            this.Controls.Add(this.onButton);
+            this.MaximizeBox = false;
+            this.Name = "NSPass";
+            this.Text = "NSPass";
+            this.Load += new System.EventHandler(this.NSPass_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button onButton;
+        private System.Windows.Forms.TextBox domainBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox consoleBox;
+        private System.Windows.Forms.TextBox serverBox;
+        private System.Windows.Forms.TextBox portBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label blockCheck;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
+
---
+++ NSPass/NSPass/NSPass.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.IO;
+using System.Diagnostics;
+
+namespace NSPass
+{
+    public partial class NSPass : Form
+    {
+        public NSPass()
+        {
+            InitializeComponent();
+        }
+
+        static TcpClient client = null;
+        private void ClientConnect(string server, int port)
+        {
+            client = new TcpClient(server, port);
+        }
+
+        private void ClientMesage(string message, string server, int port)
+        {
+            if (message == null || message == "")
+            {
+                Logg("Invalid Domain");
+                client.Close();
+                return;
+            }
+
+            byte[] data = Encoding.ASCII.GetBytes(message);
+
+            NetworkStream stream = client.GetStream();
+
+            stream.Write(data, 0, data.Length);
+            Logg(" {CONNECTION} Server Address: " + server + " Port: " + port);
+            Logg("Sent: " + message);
+
+            data = new Byte[256];
+
+            try
+            {
+                int bytes = stream.Read(data, 0, data.Length);
+                string responseData = Encoding.ASCII.GetString(data, 0, bytes);
+
+                if (responseData == "0")
+                {
+                    Logg("Could not connect to DNS Server");
+                    client.Close();
+                    return;
+                }
+                if (WriteToStor(responseData, message))
+                {
+                    Logg("Writing Completed");
+                } else
+                {
+                    Logg("{ERROR} Writing Failed");
+                }
+                Logg(Color.Green, " {RECEIVING} DNS Server Address:" + responseData + " For Domain: " + message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Logg("{ERROR} Writing Error, Denied Access");
+                client.Close();
+                return;
+            }
+
+            client.Close();
+        }
+
+        private bool ControlChecks()
+        {
+            if (serverBox.Text == null || serverBox.Text == "")
+            {
+                Logg("Invalid NSPass Server Address");
+                return false;
+            }
+
+            if (domainBox.Text == null || domainBox.Text == "")
+            {
+                Logg("Invalid Domain");
+                return false;
+            }
+
+            if (portBox.Text == null || portBox.Text == "")
+            {
+                Logg("Invalid NSPass Server Port");
+                return false;
+            }
+            return true;
+        }
+
+        private void onButton_Click(object sender, EventArgs e)
+        {
+            if (ControlChecks())
+            {
+                string address = serverBox.Text;
+                int port = int.Parse(portBox.Text);
+                string domain = domainBox.Text;
+
+                BlockedCheck(domain);
+
+                Logg("Starting... ");
+                ClientConnect(address, port);
+                ClientMesage(domain, address, port);
+
+                string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "nspass.cache");
+                CacheObj cache = new CacheObj
+                {
+                    Server = address,
+                    Port = port.ToString(),
+                    Domain = domain,
+                };
+
+                File.WriteAllText(path, Newtonsoft.Json.JsonConvert.SerializeObject(cache, Newtonsoft.Json.Formatting.Indented));
+            }
+        }
+
+        void Logg(params string[] messages)
+        {
+            foreach (string message in messages)
+            {
+                consoleBox.Text += "\n[" + DateTime.Now.ToString() + "] " + message + " ";
+            }
+        }
+
+        void Logg(Color color, string message)
+        {
+            consoleBox.SelectionStart = consoleBox.TextLength;
+            consoleBox.SelectionLength = 0;
+
+            consoleBox.SelectionColor = color;
+            consoleBox.SelectionFont = new Font(consoleBox.Font, FontStyle.Bold);
+
+            consoleBox.AppendText("\n[" + DateTime.Now.ToString() + "] " + message + " ");
+            consoleBox.SelectionColor = consoleBox.ForeColor;
+        }
+
+        private bool WriteToStor(string address, string domain)
+        {
+            string currentTime = DateTime.Now.ToString();
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "hosts.txt");
+            string previousTxt = File.ReadAllText(path);
+
+            string writeTxt = String.Format("{0}\t{1}", address, domain);
+
+            Logg(path);
+            
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    File.Create(path);
+                }
+                
+                if (previousTxt.Contains(writeTxt))
+                {
+                    return true;
+                }
+                else
+                {
+                    File.WriteAllText(path, previousTxt + "\n\n# " + currentTime + "\n\n" + writeTxt);
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            
+        }
+
+        static bool blocked;
+
+        void BlockedCheck(string domain)
+        {
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+
+            options.DontFragment = true;
+
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+
+            try
+            {
+                PingReply reply = pingSender.Send(domain, timeout, buffer, options);
+                if (reply.Status == IPStatus.Success)
+                {
+                    blocked = false;
+                }
+                else
+                {
+                    blocked = true;
+                }
+            }
+            catch
+            {
+                blocked = true;
+            }
+
+            if (blocked)
+            {
+                blockCheck.ForeColor = Color.Red;
+                blockCheck.Text = "Blocked";
+            } else
+            {
+                blockCheck.ForeColor = Color.Green;
+                blockCheck.Text = "Not Blocked";
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            Stream saveStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((saveStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    string hostPath = @"C:\WINDOWS\System32\Drivers\etc\hosts";
+                    string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "hosts.txt");
+
+                    string rawTxt = File.ReadAllText(hostPath) + "\n\n" + DateTime.Now.ToString() + File.ReadAllText(path);
+                    byte[] buffer = Encoding.ASCII.GetBytes(rawTxt);
+                    saveStream.Write(buffer, 0, buffer.Length);
+
+                    saveStream.Close();
+                }
+            }
+        }
+
+        private void NSPass_Load(object sender, EventArgs e)
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "nspass.cache");
+            if (!File.Exists(path))
+            {
+                File.CreateText(path);
+            } 
+            else if (File.ReadAllText(path) == "" || File.ReadAllText(path) == null)
+            {
+
+            }
+            else
+            {
+                string rawTxt = File.ReadAllText(path);
+                CacheObj cache = Newtonsoft.Json.JsonConvert.DeserializeObject<CacheObj>(rawTxt);
+
+                serverBox.Text = cache.Server;
+                portBox.Text = cache.Port;
+                domainBox.Text = cache.Domain;
+            }
+        }
+    }
+
+    public class CacheObj 
+    { 
+        public string Server { get; set; }
+        public string Port { get; set; }
+        public string Domain { get; set; }
+    }
+
+}
